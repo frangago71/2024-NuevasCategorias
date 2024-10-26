f7f00834db3df3e821279a7d488275692ac1d042
Francisco Gago <frangago71@gmail.com>
2024-10-26 19:23:54 +0200

    Backend

--- DeliverUS-Backend/src/routes/RestaurantCategoryRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantCategoryRoutes.js
@@ -1,7 +1,16 @@
 import RestaurantCategoryController from '../controllers/RestaurantCategoryController.js'
+import { handleValidation } from '../middlewares/ValidationHandlingMiddleware.js'
+import { isLoggedIn, hasRole } from '../middlewares/AuthMiddleware.js'
+import * as RestaurantCategoryValidation from '../controllers/validation/RestaurantCategoryValidation.js'
 
 const loadFileRoutes = function (app) {
   app.route('/restaurantCategories')
     .get(RestaurantCategoryController.index)
+    .post(
+      isLoggedIn,
+      hasRole('owner'),
+      handleValidation,
+      RestaurantCategoryValidation.create,
+      RestaurantCategoryController.create)
 }
 export default loadFileRoutes
